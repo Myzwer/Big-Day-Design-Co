2e3f02e87b3cfb48ccee87145cd4d3afc97b846f
Myzwer <josh@onefortyfivedesign.com>
2022-10-11 01:19:18 -0400

    :art: Updated colors to be brand appropriate.

--- webpack/tailwind.config.js
+++ webpack/tailwind.config.js
@@ -34,29 +34,24 @@ module.exports = {
        *
        * https://tailwindcss.com/docs/customizing-colors#custom-colors
        * */
-      blue: {
-        light: "#6495ED", // text-blue-light
-        DEFAULT: "#0047AB", // text-blue
-        dark: "#00008B", // text-blue-dark
-        100: "#ff0000", // text-blue-100
-      },
-      pink: {
-        light: "#ff7ce5",
-        DEFAULT: "#ff49db",
-        dark: "#ff16d1",
-      },
-      gray: {
-        darkest: "#1f2d3d",
-        dark: "#3c4858",
-        DEFAULT: "#c0ccda",
-        light: "#e0e6ed",
-        lightest: "#f9fafc",
-      },
       white: {
-        DEFAULT: "#ffffff",
+        DEFAULT: "#eae5df",
       },
       black: {
-        DEFAULT: "#000000",
+        DEFAULT: "#333333",
+      },
+      charcoal: {
+        DEFAULT: "#333333",
+      },
+      tan: {
+        DEFAULT: "#eae5df",
+      },
+      silver: {
+        DEFAULT: "#bfbcbf",
+        dark: "#747884",
+      },
+      peach: {
+        DEFAULT: "#ef9273",
       },
     },
   },
