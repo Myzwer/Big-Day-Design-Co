7d55eaca8376b4f006274bc5e484d057d4658a7e
Myzwer <josh@onefortyfivedesign.com>
2022-10-11 23:23:22 -0400

    :bug: Updated the template name because I forgot to do it earlier.

--- site-templates.php
+++ site-templates.php
@@ -1,1 +1,17 @@
 <?php
+/**
+ * Template Name: The Templates
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Big_Day_Design
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    // Content
+
+<?php get_footer();
