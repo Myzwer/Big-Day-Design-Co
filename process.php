ae018ea2f96df7703fc2f108270ded0cfb377d36
Myzwer <josh@onefortyfivedesign.com>
2022-10-11 23:09:41 -0400

    :heavy_plus_sign: Added the new php templates for the other pages.

---
+++ process.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Process
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Big_Day_Design
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+// Content
+
+<?php get_footer();
