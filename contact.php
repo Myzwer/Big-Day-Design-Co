a004019778168270c5a99e3ed43b467dfa9a1cac
Myzwer <josh@onefortyfivedesign.com>
2022-10-21 16:25:32 -0400

    :rocket: Knocked out the contact page design.

--- contact.php
+++ contact.php
@@ -11,69 +11,110 @@
  */
 
 get_header(); ?>
-<div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
-  rgba(0, 0, 0, 0.45),
-  rgba(0, 0, 0, 0.45)
-), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
- height: 40vh;">
-    <div class="content-middle text-white text-center">
-        <h1 class="text-4xl md:text-5xl mb-3">Contact</h1>
+
+    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+            rgba(0, 0, 0, 0.6),
+            rgba(0, 0, 0, 0.6)
+            ), url('<?php the_field('header_image') ?>') center center; background-repeat: no-repeat; background-size: cover;
+            height: 60vh;">
+        <div class="content-middle text-white text-center">
+            <h1 class="text-4xl mb-7 px-2">You plan happily ever after. I've got it from here.</h1>
+        </div>
     </div>
-</div>
 
-<div class="bg-white mb-10">
-    <div class="mx-4 md:mx-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
-        <div class="grid grid-cols-12 gap-4">
-            <div class="col-span-12 card-gradient-1 rounded-xl shadow-2xl mb-10">
-                <div class="text-center p-5 md:p-10 text-black">
-                    <h2 class="text-3xl text-left">Want to Contact Us?</h2>
-                    <p class="text-left mb-10">I'm baby irony ramps shoreditch microdosing, raclette tattooed gentrify prism art party gastropub woke quinoa. Vape bitters kale chips everyday carry asymmetrical. Austin hot chicken 90's tousled woke tattooed pabst prism art party meditation hell of slow-carb. Seitan tofu mlkshk asymmetrical 8-bit letterpress chambray brooklyn taiyaki flexitarian tumeric truffaut. Everyday carry hell of sriracha, listicle brooklyn unicorn helvetica quinoa. Helvetica hexagon DIY before they sold out cray, cloud bread church-key.</p>
-                    <a href=""
-                       class="bg-orange rounded-full font-bold shadow-xl text-black px-8 py-3 transition duration-300 ease-in-out hover:bg-orange-hover">
-                        Make Magic
-                    </a>
-                </div>
-            </div>
-
-            <div class="col-span-12 md:col-span-8 card-gradient-1 rounded-xl shadow-2xl">
-                <div class="text-left p-10 text-black form">
-
-                    <!-- This will generate your form when you add it in WP Admin. -->
-                    <?php if (have_posts()) : while (have_posts()) : the_post();
-                        the_content();
-                    endwhile;
-                    else: ?>
-                        <p>Sorry, no posts matched your criteria.</p>
-                    <?php endif; ?>
-
-                </div>
-            </div>
-
-            <div class="col-span-12 md:col-span-4">
-                <div class="grid grid-cols-12 gap-4">
+    <div class="bg-rose-gradient text-white">
+        <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+                rgba(0, 0, 0, 0.0),
+                rgba(0, 0, 0, 0.0)
+                ), url('<?php the_field('waves_background') ?>') center center;  background-repeat: no-repeat; background-size: cover;">
+            <div class="mx-4 md:mx-10 lg:max-w-3xl lg:mx-auto py-10">
+                <div class="grid grid-cols-12 gap-4 md:gap-10">
                     <div class="col-span-12">
-                        <div class="card-gradient-1 rounded-xl shadow-2xl">
-                            <div class="text-left p-10 text-black">
-                                <h2 class="text-3xl text-left">Get In Touch</h2>
-                                <p class="text-left"><i class="fas fa-phone text-xl"></i> 123-456-7890</p>
-                                <p class="text-left"><i class="far fa-envelope text-xl"></i> email@domain.com</p>
-                            </div>
-                        </div>
+                        <h2 class="capitalize text-3xl font-bold text-center">Let's Connect.</h2>
                     </div>
 
                     <div class="col-span-12">
-                        <div class="card-gradient-1 rounded-xl shadow-2xl">
-                            <div class="text-left p-10 text-black">
-                                <h2 class="text-3xl text-left">Second Block</h2>
-                                <p class="text-left font-bold leading-5">Want like a job or something?</p>
-                                <p class="text-left"><i class="far fa-envelope"></i> email@domain.com</p>
-                            </div>
-                        </div>
+                        <!-- This will generate your form when you add it in WP Admin. -->
+                        <?php if (have_posts()) : while (have_posts()) : the_post();
+                            the_content();
+                        endwhile;
+                        else: ?>
+                            <p>Sorry, no posts matched your criteria.</p>
+                        <?php endif; ?>
                     </div>
+
+
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="bg-blue-gradient text-black">
+        <div class="mx-4 md:mx-10 lg:max-w-5xl lg:mx-auto py-10">
+            <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-12">
+                    <h2 class="capitalize text-3xl font-bold text-center">Frequently Asked Questions</h2>
+                </div>
+
+                <div class="col-span-12">
+                    <div class="py-5">
+                        <h5 class="uppercase font-bold text-xl">How long until I get my website?</h5>
+                        <p>After the initall call, you'll sign the contract and fill out the form. Unless you are told
+                            otherwise during contract signing, <span class="font-bold"> plan on 2 weeks from when you hit submit on the form</span>
+                            until you get your website.</p>
+                    </div>
+
+                    <hr>
+
+                    <div class="py-5">
+                        <h5 class="uppercase font-bold text-xl">Can I edit my website myself?</h5>
+                        <p>After you are sent your website, there is a period in which you can request changes to any
+                            text, colors, or images. Since you already know exactly what you are getting in your
+                            template, I typically do not allow any visual changes to your site. It already looks great,
+                            I promise!</p>
+                    </div>
+
+                    <hr>
+
+                    <div class="py-5">
+                        <h5 class="uppercase font-bold text-xl">Who else is using the templates?</h5>
+                        <p>The templates are designed and built by me for my customers. No one else is using them or has
+                            them, so they are unique to you! If another couple chooses the same template as you, then
+                            those sites would look similar, but you won't find anything like this on Squarespace /
+                            WordPress / The Knot / etc.</p>
+                    </div>
+
+                    <hr>
+
+                    <div class="py-5">
+                        <h5 class="uppercase font-bold text-xl">How does this form work?</h5>
+                        <p>The form is designed to be as fun for you as possible. Your website needs content, and the
+                            form helps me get the information I need without being difficult for you. There are plenty
+                            of sections where it will ask if you want to write the text yourself, but every section has
+                            options to answer questions and I'll write it for you!</p>
+                    </div>
+
+                    <hr>
+
+                    <div class="py-5">
+                        <h5 class="uppercase font-bold text-xl">We don't have many good photos of us :(</h5>
+                        <p>Not to worry! I will use what photos you do have where appropriate, and fill in stock photos
+                            where there are gaps.</p>
+                    </div>
+
+                </div>
+
+                <div class="col-span-12 ">
+                    <h5 class="uppercase font-bold text-xl pb-5">Still have questions?</h5>
+                    <a href="mailto:josh@onefortyfivedesign.com"
+                       class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover mt-10">
+                        Email me
+                    </a>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
 
 <?php get_footer();
