a5c248c456c6fdc3d8fd9d6b40b2817108ee4400
Myzwer <josh@onefortyfivedesign.com>
2022-10-21 16:46:52 -0400

    :rocket: Finished the privacy policy page.

--- privacy-policy.php
+++ privacy-policy.php
@@ -14,12 +14,13 @@
 get_header(); ?>
 
     <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
-  rgba(0, 0, 0, 0.45),
-  rgba(0, 0, 0, 0.45)
-), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
- height: 60vh;">
+            rgba(0, 0, 0, 0.6),
+            rgba(0, 0, 0, 0.6)
+            ), url('<?php the_field('header_image') ?>') center center; background-repeat: no-repeat; background-size: cover;
+            height: 60vh;">
         <div class="content-middle text-white text-center">
-            <h1 class="text-4xl mb-5">Privacy Policy</h1>
+            <h1 class="text-4xl mb-7 px-2">Privacy Policy. Reading this takes longer than my entire process.</h1>
+            <h2 class="text-md font-bold uppercase body-font">(For legal reasons, this is a joke)</h2>
         </div>
     </div>
 
