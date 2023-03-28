bfc1d52f97f02b252ce17ca4246ef8716cf71e78
Myzwer <josh@onefortyfivedesign.com>
2023-03-28 00:30:21 -0400

    :construction: Working on the design of this page, but I've swapped out some of the colors and the images.

--- site-templates.php
+++ site-templates.php
@@ -18,141 +18,136 @@ get_header(); ?>
             ), url('<?php the_field('header_image') ?>') center center; background-repeat: no-repeat; background-size: cover;
             height: 60vh;">
         <div class="content-middle text-white text-center">
-            <h1 class="text-4xl mb-7 px-2">Know exactly what you are getting</h1>
+            <h1 class="text-5xl mb-2 px-2">Pick Your Template</h1>
+            <h2 class="text-2xl  mb-5 font-bold uppercase body-font">Know exactly what you are getting</h2>
             <a href="/contact"
                class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover mt-10">
-                Schedule My Free Consultation
+                Start The Process
             </a>
         </div>
     </div>
 
-    <div class="bg-blue-gradient text-black">
-        <div class="mx-4 md:mx-10 lg:max-w-5xl lg:mx-auto py-10">
-            <div class="grid grid-cols-12 gap-4">
-                <div class="col-span-12 md:col-span-6">
-                    <div class="text-center md:py-5 font-bold uppercase">
-                        <img class="shadow-lg rounded-xl" src="<?php the_field('template_image') ?> "
-                             alt="Template Images">
-                    </div>
-                </div>
-                <div class="col-span-12 md:col-span-6 md:ml-10 relative">
-                    <div class="content-middle-medium">
-                        <div class="pb-5 font-bold">
-                            <h2 class="text-2xl text-left uppercase pb-3">3 elegant options, with all the features you
-                                want
-                                and none you don’t.</h2>
-                            <ul class="list-disc pl-5">
-                                <li>All 3 templates are available to you for free</li>
-                                <li>Pick and choose the pages you want</li>
-                                <li>Templates are custom for my clients, no one else has these</li>
-                            </ul>
-                        </div>
-                        <a href="/contact"
-                           class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover">
-                            Start the Process
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="bg-rose-gradient text-white">
+    <div class="bg-purple-gradient text-white">
         <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
                 rgba(0, 0, 0, 0.0),
                 rgba(0, 0, 0, 0.0)
                 ), url('<?php the_field('waves_background') ?>') center center;  background-repeat: no-repeat; background-size: cover;">
             <div class="mx-4 md:mx-10 lg:max-w-5xl lg:mx-auto py-10">
-                <div class="grid grid-cols-12 gap-4 md:gap-10">
-                    <div class="col-span-12">
-                        <h2 class="capitalize text-3xl font-bold text-center">Imagine your guests reaction to your
-                            site.</h2>
-                    </div>
+                <div class="grid grid-cols-12 gap-4">
+                    <div class="col-span-12 md:ml-10 relative">
+                        <div class="pb-5 font-bold md:text-center">
+                            <div class="mb-8">
+                                <h2 class="text-3xl uppercase pb-2 text-center">3 elegant options</h2>
+                                <h4 class="text-xl uppercase pb-5 font-bold">All the features you want and none you
+                                    don’t</h4>
 
-                    <div class="col-span-12">
-                        <div class="grid grid-cols-12 gap-4 md:gap-10">
-                            <div class="col-span-12 md:col-span-6">
-                                <img class="rounded-lg shadow-lg" src="<?php the_field('template_1') ?>"
-                                     alt="Template 1">
+                                <p>All 3 templates are available to you for free</p>
+                                <p>Pick and choose the pages you want</p>
+                                <p>Templates are custom for my clients, no one else has these</p>
+
                             </div>
-                            <div class="col-span-12 md:col-span-6 relative">
-                                <div class="content-middle-medium">
-                                    <h4 class="capitalize text-2xl font-bold">Template 1: Coffee</h4>
-                                    <ul class="list-disc pl-5  mb-5">
-                                        <li>Minimalist design</li>
-                                        <li>Good for those who don’t have a lot of photos</li>
-                                        <li>Good for those who don’t want to say a lot</li>
-                                    </ul>
-                                    <a href="#"
-                                       class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover">
-                                        <i class="fa-solid fa-arrow-up-right-from-square"></i> View Demo Site
-                                    </a>
-                                </div>
-                            </div>
+                            <a href="/contact"
+                               class="bg-rose uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-rose-hover">
+                                Start the Process
+                            </a>
                         </div>
                     </div>
-
-                    <div class="col-span-12 md:hidden">
-                        <hr class = "my-10">
-                    </div>
-
-                    <div class="col-span-12">
-                        <div class="grid grid-cols-12 gap-4 md:gap-10">
-                            <div class="col-span-12 md:col-span-6">
-                                <img class="rounded-lg shadow-lg" src="<?php the_field('template_2') ?>"
-                                     alt="Template 1">
-                            </div>
-                            <div class="col-span-12 md:col-span-6 relative">
-                                <div class="content-middle-medium">
-                                    <h4 class="capitalize text-2xl font-bold">Template 1: Coffee</h4>
-                                    <ul class="list-disc pl-5  mb-5">
-                                        <li>Minimalist design</li>
-                                        <li>Good for those who don’t have a lot of photos</li>
-                                        <li>Good for those who don’t want to say a lot</li>
-                                    </ul>
-                                    <a href="#"
-                                       class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover">
-                                        <i class="fa-solid fa-arrow-up-right-from-square"></i> View Demo Site
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-span-12 md:hidden">
-                        <hr class = "my-10">
-                    </div>
-
-                    <div class="col-span-12">
-                        <div class="grid grid-cols-12 gap-4 md:gap-10">
-                            <div class="col-span-12 md:col-span-6">
-                                <img class="rounded-lg shadow-lg" src="<?php the_field('template_3') ?>"
-                                     alt="Template 1">
-                            </div>
-                            <div class="col-span-12 md:col-span-6 relative">
-                                <div class="content-middle-medium">
-                                    <h4 class="capitalize text-2xl font-bold">Template 1: Coffee</h4>
-                                    <ul class="list-disc pl-5  mb-5">
-                                        <li>Minimalist design</li>
-                                        <li>Good for those who don’t have a lot of photos</li>
-                                        <li>Good for those who don’t want to say a lot</li>
-                                    </ul>
-                                    <a href="#"
-                                       class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover">
-                                        <i class="fa-solid fa-arrow-up-right-from-square"></i> View Demo Site
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
     </div>
 
+
     <div class="bg-blue-gradient text-black">
+        <div class="mx-4 md:mx-10 lg:max-w-5xl lg:mx-auto py-10">
+            <div class="grid grid-cols-12 gap-4 md:gap-10">
+                <div class="col-span-12">
+                    <h2 class="capitalize text-3xl font-bold text-center">Imagine your guests reaction to your
+                        site</h2>
+                </div>
+
+                <div class="col-span-12">
+                    <div class="grid grid-cols-12 gap-4 md:gap-10">
+                        <div class="col-span-12 md:col-span-6">
+                            <img src="<?php the_field('template_1') ?>"
+                                 alt="Template 1">
+                        </div>
+                        <div class="col-span-12 md:col-span-6 relative">
+                            <div class="content-middle-medium">
+                                <h4 class="capitalize text-2xl font-bold">Template 1: Coffee</h4>
+                                <ul class="list-disc pl-5  mb-5">
+                                    <li>Minimalist design</li>
+                                    <li>Good for those who don’t have a lot of photos</li>
+                                    <li>Good for those who don’t want to say a lot</li>
+                                </ul>
+                                <a href="#"
+                                   class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i> View Demo Site
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-span-12 md:hidden">
+                    <hr class="my-10">
+                </div>
+
+                <div class="col-span-12">
+                    <div class="grid grid-cols-12 gap-4 md:gap-10">
+                        <div class="col-span-12 md:col-span-6">
+                            <img src="<?php the_field('template_2') ?>"
+                                 alt="Template 1">
+                        </div>
+                        <div class="col-span-12 md:col-span-6 relative">
+                            <div class="content-middle-medium">
+                                <h4 class="capitalize text-2xl font-bold">Template 1: Coffee</h4>
+                                <ul class="list-disc pl-5  mb-5">
+                                    <li>Minimalist design</li>
+                                    <li>Good for those who don’t have a lot of photos</li>
+                                    <li>Good for those who don’t want to say a lot</li>
+                                </ul>
+                                <a href="#"
+                                   class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i> View Demo Site
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-span-12 md:hidden">
+                    <hr class="my-10">
+                </div>
+
+                <div class="col-span-12">
+                    <div class="grid grid-cols-12 gap-4 md:gap-10">
+                        <div class="col-span-12 md:col-span-6">
+                            <img src="<?php the_field('template_3') ?>"
+                                 alt="Template 3">
+                        </div>
+                        <div class="col-span-12 md:col-span-6 relative">
+                            <div class="content-middle-medium">
+                                <h4 class="capitalize text-2xl font-bold">Template 3: Lersaat</h4>
+                                <ul class="list-disc pl-5 mb-5">
+                                    <li>Minimalist design</li>
+                                    <li>Good for those who don’t have a lot of photos</li>
+                                    <li>Good for those who don’t want to say a lot</li>
+                                </ul>
+                                <a href="#"
+                                   class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover">
+                                    <i class="fa-solid fa-arrow-up-right-from-square"></i> View Demo Site
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-purple-gradient text-white">
         <div class="mx-4 md:mx-10 lg:max-w-7xl lg:mx-auto py-10">
             <div class="grid grid-cols-12 gap-4">
                 <div class="col-span-12 pb-10">
