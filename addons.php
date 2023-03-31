bb22af73357562438e3bb305a53f67f72523a99a
Myzwer <josh@onefortyfivedesign.com>
2023-03-31 01:57:00 -0400

    :white_check_mark: Created an add ons page to RSVP's and digital downloads

---
+++ addons.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Add Ons
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Big_Day_Design
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+
+
+
+<?php get_footer();
