56c84b32b9661ad3b4f42f451a23c2bf2eb64eca
Myzwer <josh@onefortyfivedesign.com>
2023-04-01 02:01:13 -0400

    :art: Still not in love with it but the pricing page is done. For now.

--- pricing.php
+++ pricing.php
@@ -12,73 +12,107 @@
 
 get_header(); ?>
 
-<div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
-            rgba(0, 0, 0, 0.7),
-            rgba(0, 0, 0, 0.7)
-            ), url('<?php the_field('pricing_image') ?>') center center;  background-repeat: no-repeat; background-size: cover;">
-        <div class="mx-4 md:mx-10 lg:max-w-5xl lg:mx-auto py-20">
-            <div class="grid grid-cols-12 gap-4 md:gap-10">
-                <div class="col-span-12">
-                    <h2 class="capitalize text-3xl font-bold text-center text-white pb-5">Simple, Upfront Pricing</h2>
+    <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+            rgba(0, 0, 0, 0.6),
+            rgba(0, 0, 0, 0.6)
+            ), url('<?php the_field('header_image') ?>') center center; background-repeat: no-repeat; background-size: cover;
+            height: 60vh;">
+        <div class="content-middle text-white text-center">
+            <h1 class="text-4xl mb-7 px-2 capitalize">Simple, Upfront Pricing</h1>
+        </div>
+    </div>
+
+    <div class="bg-purple-gradient">
+        <div class="grid grid-cols-12 gap-4 py-8 max-w-6xl text-center mx-auto">
+            <div class="col-span-12 bg-rose-gradient text-white rounded-xl shadow-xl mx-5">
+                <h2 class="text-5xl text-left uppercase pb-3 font-bold text-center pt-5 px-2">$750</h2>
+                <p class="text-2xl text-left uppercase text-center body-font font-bold mb-5">That's it. No hidden
+                    fees.</p>
+                <hr class="mx-5">
+
+
+                <div class="grid grid-cols-12 gap-4 text-left px-10">
+                    <div class="col-span-12 md:col-span-6">
+                        <h3 class="text-lg uppercase font-bold body-font pt-5">Website</h3>
+                        <p>Choice of three professionally-designed templates</p>
+                        <p>Use your own wedding color palette</p>
+                        <p>We can write all the text for your site</p>
+                        <p>We can use free to you stock photos where needed</p>
+                        <p>Custom URL - Easy for guests (and you!) to remember</p>
+                        <p>Website hosting until your wedding day</p>
+                    </div>
+
+                    <div class="col-span-12 md:col-span-6">
+                        <h3 class="text-lg uppercase font-bold body-font pt-5">Pages</h3>
+                        <p class="text-sm  pb-2 font-bold">Your Choice of...</p>
+                        <p>Homepage</p>
+                        <p>Meet the Groom</p>
+                        <p>Meet the Bride</p>
+                        <p>Meet the Couple</p>
+                        <p>Meet the Bridesmaids</p>
+                        <p>Meet the Groomsmen</p>
+                        <p>The Venue</p>
+                        <p>Schedule</p>
+                        <p>Registry</p>
+                        <p>Contact</p>
+                    </div>
+
+                    <div class="col-span-12">
+                        <div class="my-10 text-center">
+                            <a href="/contact"
+                               class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover mt-10">
+                                Start the Process
+                            </a>
+                        </div>
+                    </div>
                 </div>
+            </div>
 
 
-                <div class="bg-blue-gradient col-span-12 md:col-span-4 rounded-xl px-5">
-                    <h2 class="text-2xl text-left uppercase pb-3 font-bold text-center pt-4 px-2">All Tiers Include</h2>
-                    <h3 class="text-lg text-left uppercase font-bold text-center body-font">website</h3>
-                    <ul class="list-disc pl-5  mb-5">
-                        <li>Choice of any tempate</li>
-                        <li>Wedding Color Palette</li>
-                        <li>Custom URL</li>
-                        <li>Website hosting until your wedding day</li>
-                    </ul>
+            <div class="col-span-12 lg:col-span-6 bg-blue-gradient rounded-xl shadow-xl relative mx-5">
+                <h2 class="text-5xl text-left uppercase pb-3 font-bold text-center pt-5 px-2">+$250</h2>
+                <p class="text-2xl text-left uppercase text-center body-font font-bold mb-5">RSVP Feature</p>
+                <hr class="mx-5">
+                <div class="text-left px-5">
+                    <h3 class="text-lg text-left uppercase font-bold body-font pt-5">What you get</h3>
+                    <p>Dedicated RSVP page</p>
+                    <p>Customizable form - Unlimited Options</p>
+                    <p>Simple form for guests</p>
+                    <p>Track guest meal preference</p>
+                    <p>Real-time email notifications</p>
+                    <p>Full guest list at your fingertips</p>
+                    <p>Print guest list for day of</p>
+                    <p>Collect addresses for Thank You notes</p>
 
-                    <h3 class="text-lg text-left uppercase font-bold text-center body-font">Pages</h3>
-                    <p class="text-sm text-center pb-2">Your Choice of...</p>
-                    <ul class="list-disc pl-5 pb-5">
-                        <li>Home</li>
-                        <li>Bride / Groom Bio</li>
-                        <li>Couple Bio</li>
-                        <li>Wedding Party Info</li>
-                        <li>Venue Details</li>
-                        <li>Lodging</li>
-                        <li>Schedule</li>
-                        <li>Contact</li>
-                        <li>Registry Directory</li>
-                    </ul>
+                    <div class="text-center my-10">
+                        <a href="/add-ons"
+                           class="border-2 border-rose uppercase rounded-md font-bold text-rose px-8 py-3 transition duration-300 ease-in-out hover:bg-rose-hover hover:text-white">
+                            Show Me
+                        </a>
+                    </div>
                 </div>
+            </div>
 
-                <div class="col-span-12 md:col-span-8">
-                    <div class="grid grid-cols-12 gap-4">
-                        <div class="col-span-12">
-                            <div class="bg-blue-gradient rounded-xl shadow-xl price-card relative">
-                                <div class="content-middle-medium py-10 md:py-0">
-                                    <h3 class="text-2xl text-left uppercase font-bold text-center">BASE</h3>
-                                    <h4 class="text-lg text-left uppercase font-bold text-center body-font">$750</h4>
-                                </div>
-                            </div>
-                        </div>
+            <div class="col-span-12 lg:col-span-6 bg-blue-gradient rounded-xl shadow-xl relative mx-5">
+                <h2 class="text-5xl text-left uppercase pb-3 font-bold text-center pt-5 px-2">+$250</h2>
+                <p class="text-2xl text-left uppercase text-center body-font font-bold mb-5">Photo Downloader</p>
+                <hr class="mx-5">
+                <div class="text-left px-5">
+                    <h3 class="text-lg text-left uppercase font-bold body-font pt-5">What you get</h3>
+                    <p>Goes live after your wedding day</p>
+                    <p>Homepage converts to have all your photos</p>
+                    <p>We import them for you</p>
+                    <p>Convenient download buttons for easy access</p>
+                    <p>No permissions or Dropbox/Google Drive tutorials required</p>
+                    <p>Custom URL ensures easy access for guests</p>
+                    <p>Keep your site for 6 months after the wedding</p>
+                    <p>Share with family and friends who couldn't attend</p>
 
-                        <div class="col-span-12">
-                            <div class="bg-rose-gradient rounded-xl shadow-xl price-card relative text-white">
-                                <div class="content-middle-medium py-10 md:py-0">
-                                    <h3 class="text-2xl text-left uppercase font-bold text-center">standard</h3>
-                                    <h4 class="text-lg text-left uppercase font-bold text-center body-font">$1000</h4>
-                                    <p class="text-center font-bold">Everything in Base + RSVP Feature</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-span-12">
-                            <div class="bg-blue-gradient rounded-xl shadow-xl price-card relative">
-                                <div class="content-middle-medium py-10 md:py-0">
-                                    <h3 class="text-2xl text-left uppercase font-bold text-center">extended</h3>
-                                    <h4 class="text-lg text-left uppercase font-bold text-center body-font">$1250</h4>
-                                    <p class="text-center font-bold">Everything in Standard + Post-wedding photo
-                                        downloads</p>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="text-center my-10">
+                        <a href="/add-ons"
+                           class="border-2 border-rose uppercase rounded-md font-bold text-rose px-8 py-3 transition duration-300 ease-in-out hover:bg-rose-hover hover:text-white">
+                            Show Me
+                        </a>
                     </div>
                 </div>
             </div>
